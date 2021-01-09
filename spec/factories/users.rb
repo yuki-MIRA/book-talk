67c7f79d2ba09cee82439db2f2ffd5c5298c9602
yuki-MIRA <oyuuki0423@gmail.com>
2021-01-09 12:08:49 +0900

    テストコード実装

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {"test"}
+    email                 {Faker::Internet.free_email}
+    password              {"a12345"}
+    password_confirmation {password}
+  end
+end
