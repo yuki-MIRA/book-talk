85c6e4d6e5d71023d8381ecd0c206717aa4f2f88
yuki-MIRA <oyuuki0423@gmail.com>
2021-01-11 16:46:01 +0900

    booksコントローラ、new,createアクション定義

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,26 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!, only: :new
+
   def index
   end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:name, :text, :genre_id, :image).merge(user_id: current_user.id)
+  end
+  
 end
