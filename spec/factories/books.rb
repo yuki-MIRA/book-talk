8ca7dd860d63d86d64f2fbaa1164eeb7c97e74e9
yuki-MIRA <oyuuki0423@gmail.com>
2021-01-12 20:27:21 +0900

    bookモデルテストコード

---
+++ spec/factories/books.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :book do
+    association :user
+
+    name {'テスト小説'}
+    text {'テスト投稿'}
+    genre_id { 2 }
+
+    after(:build) do |book|
+      book.image.attach(io: File.open('public/images/hero.jpg'), filename: 'hero.jpg')
+    end
+  end
+end
