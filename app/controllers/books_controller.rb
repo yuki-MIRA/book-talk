7af33c910e4215e217831fe607a565eef8d88409
yuki-MIRA <oyuuki0423@gmail.com>
2021-01-17 12:29:11 +0900

    edit,updateアクション実装。編集機能の利用可否制限の実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :move_top, only: :edit
 
   def index
     @books = Book.includes(:user).order('created_at DESC')
@@ -22,9 +23,30 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def book_params
     params.require(:book).permit(:name, :text, :genre_id, :image).merge(user_id: current_user.id)
   end
+
+  def move_top
+    @book = Book.find(params[:id])
+    unless current_user.id == @book.user_id
+      redirect_to root_path
+    end
+  end
+
 end
